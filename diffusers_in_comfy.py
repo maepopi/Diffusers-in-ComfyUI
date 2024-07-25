ebcfdbd5a167e0e03ce8e70d335436f347b12e2f
Maëlys Jusseaux <https://gitlab.com/mjusseaux>
2024-07-25 21:44:25 +0200

    force controlnet scale to be considered float

--- diffusers_in_comfy.py
+++ diffusers_in_comfy.py
@@ -162,7 +162,7 @@ class ImageInference:
         
         if controlnet_image:
             args['image'] = controlnet_image
-            args['controlnet_conditioning_scale'] = controlnet_scale
+            args['controlnet_conditioning_scale'] = float(controlnet_scale)
         
         images = pipeline(**args).images
 
